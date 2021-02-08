5ceeced61f9de71ea15077e4fff11bbd7f150a77
pavankpdev <pavankumarkpkp@gmail.com>
2021-02-08 17:28:06 +0530

    card styles update

--- src/components/ImageCard/ImageCard.component.jsx
+++ src/components/ImageCard/ImageCard.component.jsx
@@ -15,28 +15,36 @@ const ImageCard = ({
     <>
       <div className="image__card__container">
         <div className="image__card__card rounded">
-          <img src={thumbnail} alt="thumbnail" />
-          <div className="image__card__card__content">
-            <h3
-              className="image__card__card__header"
-              data-toggle="tooltip"
-              title={name}
+          <div className="image__card__card__footer">
+            <div className="image__card__card__footer__contributor__info">
+              <img src={contributor.image} alt="contributor avatar" />
+              <h5>{contributor.name}</h5>
+            </div>
+          </div>
+          <div className="image__card__card__body">
+            <Link
+              to={`/resource/${id}`}
+              style={{ backgroundImage: `url(${thumbnail})` }}
+              className="thumbnail"
+            ></Link>
+            <Link
+              to={`/resource/${id}`}
+              className="image__card__card__content default"
             >
-              {name}
-            </h3>
-            <div className="image__card__card__body">
-              <p>{description}</p>
-              <Link to={`/resource/${id}`}>
-                <button className="btn btn-sm btn-primary">Learn more</button>
-              </Link>
-            </div>
-            <div className="image__card__card__footer">
-              <h5>Listed By</h5>
-              <div className="image__card__card__footer__contributor__info">
-                <img src={contributor.image} alt="contributor avatar" />
-                <h5>{contributor.name}</h5>
+              <h3
+                className="image__card__card__header"
+                data-toggle="tooltip"
+                title={name}
+              >
+                {name}
+              </h3>
+              <div className="image__card__card__description">
+                <p>{description}</p>
+                <Link to={`/resource/${id}`}>
+                  <button className="btn btn-sm btn-primary">Learn more</button>
+                </Link>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
