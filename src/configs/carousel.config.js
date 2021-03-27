d2be236b918a3c32636ab50a8129b1a2531c0459
pavankpdev <pavankumarkpkp@gmail.com>
2021-03-27 23:32:15 +0530

    cean up for v2

--- src/configs/carousel.config.js
+++
@@ -1,7 +0,0 @@
-export const carouselConfig = {
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  centerMode: true,
-  variableWidth: true,
-};
--- src/configs/reduxPersist.config.js
+++
@@ -1,8 +0,0 @@
-// libraries
-import storage from "redux-persist/lib/storage";
-
-export const persistConfig = {
-  key: "redux",
-  storage,
-  whitelist: ["user", "resources", "contributors"],
-};
